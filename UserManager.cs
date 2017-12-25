53e5e7965280afc8f476817bc8b7c27776af22b9
jpg-consulting <opensource@jpg-consulting.es>
2017-12-25 14:41:44 +0100

    Added user manager

---
+++ UserManager.cs
@@ -0,0 +1,47 @@
+﻿using InvoiceNet.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceNet.Data.Managers
+{
+    public class UserManager
+    {
+        private SqlConnection m_connection;
+
+        public GenericUser GetUser(String userName)
+        {
+            GenericUser user = null;
+            m_connection.Open();
+
+            SqlCommand command = m_connection.CreateCommand();
+            command.CommandText = "SELECT * FROM users WHERE user_name=@UserName";
+            IDbDataParameter parameter = command.CreateParameter();
+            parameter.ParameterName = "@UserName";
+            parameter.Value = userName;
+            command.Parameters.Add(parameter);
+            SqlDataReader dataReader = command.ExecuteReader();
+
+            if (dataReader.Read())
+            {
+                if (dataReader.GetString(dataReader.GetOrdinal("user_name")).Equals(userName))
+                {
+                    UInt32 uid = dataReader.GetUInt32(dataReader.GetOrdinal("user_id")));
+                    user = new GenericUser(uid, userName);
+                }
+            }
+
+            dataReader.Close();
+
+            return user;
+        }
+
+        public UserManager(SqlConnection connection)
+        {
+            m_connection = connection;
+        }
+    }
+}
